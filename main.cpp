6ff5a9d97bc6a7dd43d9745e5b7f8167a3965edb
Jhony279 <lordjhony732@gmail.comm>
2026-02-28 19:55:38 -0800

    Adding comments to lines of code

--- main.cpp
+++ main.cpp
@@ -26,14 +26,14 @@ class Movie {
     void printData() {
         cout << "Movie: " << title << endl;
         cout << "   Year Released: " << yearReleased << endl;
-        cout << "   ScreenWriter: " << writer << endl;
+        cout << "   ScreenWriter: " << writer << endl << endl;
     }
 };
 
 void insertMovieData(string, vector<Movie>&);
 
 /**
- * @brief - Description of main
+ * @brief 
  * @return 
 */
 int main() {
@@ -49,6 +49,11 @@ int main() {
     return 0;
 }
 
+/**
+ * @brief Reads movie data from a file and populates a vector of Movie objects
+ * @param filePath The path to the file containing movie data
+ * @param moviesV A reference to a vector of Movie objects to be populated with the data
+*/
 void insertMovieData(string filePath, vector<Movie>& moviesV) {
     string writer;
     int yearReleased;
@@ -58,21 +63,23 @@ void insertMovieData(string filePath, vector<Movie>& moviesV) {
     string text;
 
     inputFile.open(filePath);
+    // Check if the file was successfully opened before attempting to read from it
     if (inputFile.is_open()) {
         int i = 0;
         while (getline(inputFile, text)) {
             if (i % 3 == 0) {
-                title = text;
+                writer = text;
             } else if (i % 3 == 1) {
                 yearReleased = stoi(text);
             } else if (i % 3 == 2) {
-                writer = text;
+                title = text;
                 Movie m;
                 m.setTitle(title);
                 m.setYearReleased(yearReleased);
                 m.setWriter(writer);
                 moviesV.push_back(m);
             }
+            // Increment the line counter to keep track of which line we are on
             i++;
         }
         inputFile.close();
